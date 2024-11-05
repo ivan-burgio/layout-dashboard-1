beaa562cd2e7f9742572e7d20b20a84b1432d36d
ivan-burgio <ivan.24.burgio@gmail.com>
2024-11-05 15:57:47 -0300

    paginas base

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Buyar">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <title>Layout Web 1</title>
+    <title>Layout Web 2</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" /> <!-- Font Awesome -->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> <!-- Animaciones AOS -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- JQuery -->
